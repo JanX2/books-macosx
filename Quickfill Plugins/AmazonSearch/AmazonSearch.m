28fe5b0574d83a2fe5d68e22162ceacb63c81d0b
Jan Weiß <jan@geheimwerk.de>
2010-07-26 16:52:18 +0200

    Updating deprecated method calls in "AmazonSearch.m".

--- Quickfill Plugins/AmazonSearch/AmazonSearch.m
+++ Quickfill Plugins/AmazonSearch/AmazonSearch.m
@@ -6,7 +6,7 @@ int main (int argc, const char * argv[])
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
 	if (argc > 2)
-		[Search searchForXml:[NSString stringWithCString:argv[1]] locale:[NSString stringWithCString:argv[2]]];
+		[Search searchForXml:[[[NSProcessInfo processInfo] arguments] objectAtIndex:1] locale:[[[NSProcessInfo processInfo] arguments] objectAtIndex:2]];
 	else 
 		[Search searchForXml:@"/tmp/books-quickfill.xml" locale:@"us"];
 		
